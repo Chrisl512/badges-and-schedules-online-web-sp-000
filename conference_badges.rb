60cbe20d4b5260699177e5bbb64159e9056d6287
Christopher Luna <chris@illusionlayervfx.com>
2019-03-11 02:40:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,8 @@ def assign_rooms(array_names)
 end
 
 def printer(array_names)
-    batch_badge_creator(array_names).each do |badges|
-     puts "#{badges}"
+    batch_badge_creator(array_names)
+     #puts "#{badges}"
+     assign_rooms(array_names)
   end
 end
